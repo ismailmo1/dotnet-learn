6e320bb66b6e444a8b9bcdea9d850c911ab4a0cc
Ismail Mohammed <ismail.mohammed@amey.co.uk>
2022-12-20 17:27:14 +0000

    initial commit

---
+++ consoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿class Hello
+{
+    static void Main()
+    {
+        Pair<int, int> p = new Pair<int, int>(1, 2);
+        Console.WriteLine(p.First);
+    }
+}
+public class Pair<TFirst, TSecond>
+{
+    public TFirst First { get; }
+    public TSecond Second { get; }
+
+    public Pair(TFirst first, TSecond second) => (First, Second) = (first, second);
+}
