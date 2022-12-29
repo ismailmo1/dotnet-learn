d5c1730fd872a6cb66b7c78bdf2486f78c6d0700
Ismail Mohammed <ismailmo4@gmail.com>
2022-12-29 01:37:02 +0000

    add swagger

--- MinimalAPI/PizzaStore/Program.cs
+++ MinimalAPI/PizzaStore/Program.cs
@@ -1,9 +1,21 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(optionas => { });
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
+});
+
 var app = builder.Build();
 
-builder.Services.AddCors(optionas => { });
 app.UseCors("unique string");
-
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+});
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
