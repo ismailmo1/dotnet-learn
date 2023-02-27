5614d6e8942f9551018e0ed2a49c774adab871b4
Ismail Mohammed <ismailmo4@gmail.com>
2023-02-27 21:19:11 +0000

    xunit intro

---
+++ xunit-intro/xunit-testing/PrimeService/PrimeService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Prime.Services;
+public class PrimeService
+{
+    public bool IsPrime(int candidate)
+    {
+        if (candidate < 2)
+        {
+            return false;
+        }
+        // super inefficient way of testing for primes
+        // but im not a mathematician lol
+        for (int divisor = 2; divisor <= Math.Sqrt(candidate); divisor++)
+        {
+            if (candidate % divisor == 0)
+            {
+                // we've found a divisor so it's not prime
+                return false;
+            }
+            // no divisors found - so must be prime
+        }
+        return true;
+    }
+}
